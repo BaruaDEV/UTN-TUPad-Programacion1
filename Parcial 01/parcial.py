404657ce3a982d9897a32a52019b4fb3d5a2a613
BaruaDEV <maurobarua77@gmail.com>
2025-11-11 08:47:27 -0300

    TPI P1
    commit de trabajo integrador de programación 1

--- Parcial 01/parcial.py
+++ Parcial 01/parcial.py
@@ -1,4 +1,5 @@
 # Parcial 1 de programacion
+# Alumno: Barua, Mauro
 
 opcion_menu = [
     "1. Ingresar titulo (sin ejemplares)",
@@ -12,11 +13,11 @@ opcion_menu = [
     "9. Salir"
 ]
 
-libros = ["El señor de los anillos", "Orgullo y prejuicio", "Matar un Ruiseñor"]
-ejemplares = [5,3,7]
+libros = ["El señor de los anillos", "Orgullo y prejuicio", "Matar un Ruiseñor"] # Libros iniciales a disposicion
+ejemplares = [5,3,7] # ejemplares iniciales a disposicion
 
 while True:
-    print("::::Menu::::\n")
+    print("\n::::Menu::::\n")
     for opcion in opcion_menu:
         print (opcion)
     seleccion = input("Seleccione una opcion: ").strip()
@@ -37,19 +38,113 @@ while True:
         ejemplares.insert(posicion, 0)
 
     elif seleccion == "2":
-        pass
+        if not libros:
+            print("No hay libros disponibles. Deben exisitir libros disponibles para ingresar cantidad de ejemplares")
+            continue
+        for i, titulo in enumerate(libros):
+            print(f"{i+1}. {titulo}")
+
+        posicion = int(input("Ingrese el numero del titulo, para asignarle ejemplares: ")) -1
+
+        while posicion < 0 or posicion >= (len(libros)):
+            print("Posicion invalida, intente nuevamente")
+            posicion = int(input("Ingrese el numero del titulo, para asignarle ejemplares: ")) -1
+        
+        cantidad = int(input("Ingrese cantidad de ejemplares del titulo: "))
+        ejemplares[posicion] += cantidad
+        print(f"Actualizacion: titulo: {libros[posicion]}, ejemplares disponibles: {ejemplares[posicion]}")
+
     elif seleccion == "3":
-        pass
+        if not libros:
+            print("No hay libros disponibles. Deben exisitir libros disponibles para ingresar cantidad de ejemplares")
+            continue
+        print("\n:::::Catálogo de libros:::::\n")
+        for i, titulo in enumerate(libros):
+            print(f"{i+1}. {titulo}")
+
     elif seleccion =="4":
-        pass
+        if not libros:
+            print("No hay libros disponibles. Deben exisitir libros disponibles para consultar")
+            continue
+        print("\n:::::libros Disonibles para consulta:::::\n")
+        for i, titulo in enumerate(libros):
+            print(f"{i+1}. {titulo}")
+
+        posicion = int(input("Ingrese el numero del titulo, para consultar disponibilidad: ")) -1
+
+        while posicion < 0 or posicion >= (len(libros)):
+            print("Posicion invalida, intente nuevamente")
+            posicion = int(input("Ingrese el numero del titulo, para asignarle ejemplares: ")) -1
+        
+        print(f"\nLa disponibilidad del libro: {libros[posicion]}, es de: {ejemplares[posicion]} ejemplares\n")
+
     elif seleccion == "5":
-        pass
+        if not libros:
+            print("No hay libros disponibles. Deben exisitir libros disponibles para consultar")
+        else:
+            agotados = False
+            print("\n::: Lista de titulos agotados:::\n")
+        
+            for i in range(len(libros)):
+                if ejemplares[i] == 0:
+                    print(libros[i])
+                    agotados = True
+                
+        if not agotados:
+             print("No hay libros agotados\n")
+                    
     elif seleccion == "6":
-        pass
+        nuevo_libro = input("Ingrese un nuevo titulo/libro: ")
+        if nuevo_libro in libros:
+            print(f"El libro: {nuevo_libro}, ya esta registrado!")
+        else:
+            libros.append(nuevo_libro)
+            posicion = libros.index(nuevo_libro)
+            cantidad = int(input("Ingrese cantidad de ejemplares del libro ingresado: "))
+            ejemplares.insert(posicion, cantidad)
+            print(f"Titulo: {nuevo_libro}, agregado al catálogo con la cantidad de: {cantidad} ejemplares disponibles")
+            
     elif seleccion == "7":
-        pass
+        if not libros:
+            print("No hay libros disponibles. Deben exisitir libros disponibles para ingresar cantidad de ejemplares")
+            continue
+
+        for i, titulo in enumerate(libros):
+            print(f"{i + 1}. {titulo}")
+        
+        posicion = int(input("Ingrese numero del libro para prestamo o devolucion: ")) -1
+
+        while posicion < 0 or posicion >= (len(libros)):
+            print("Posicion invalida, intente nuevamente")
+            posicion = int(input("Ingrese el numero del titulo: ")) -1
+
+        accion = input("Ingrese 'p' para prestamo o 'd' para devolucion: ").lower()
+
+        if accion == "p":
+            if ejemplares[posicion] > 0:
+                ejemplares[posicion] -= 1
+                print(f"Prestamo realizado. Ejemplares disponibles de: {libros[posicion]}: {ejemplares[posicion]}")
+            else:
+                print(f"No hay ejemplares disponibles para {libros[posicion]}")
+
+        elif accion == "d":
+            cantidad_devolucion = int(input("Ingrese la cantidad a devolver: "))
+            if cantidad_devolucion < 0:
+                print("Error, la devolucion minima es de 1")
+            else:
+                ejemplares[posicion] += cantidad_devolucion
+                print("Devolucion realizada")
+        else:
+            print("Accion invalida. Use 'p' (prestamo) o 'd' (devolucion)")
+
     elif seleccion == "8":
-        pass
+        if not libros:
+            print("No hay libros disponibles. Deben exisitir libros disponibles para ingresar cantidad de ejemplares")
+            continue
+        print(":::: CATALOGO COMPLETO ::::\n")
+        for i, titulo in enumerate(libros):
+            print(f"{i + 1}. {titulo}, ejemplares disopnibles: {ejemplares[i]}")
+
     elif seleccion == "9":
         print("\nFinalizando programa...")
         break
---
+++ TPI_programacion/REAME.md
@@ -0,0 +1,90 @@
+# TPI Programación I — Gestión de Países
+
+Este proyecto corresponde al **Trabajo Práctico Integrador de la materia Programación I**.  
+Fue desarrollado en **Python 3** y permite gestionar información sobre distintos países utilizando un archivo CSV como base de datos.
+
+# A continuación se deja el link de acceso al video explicativo del proyecto
+
+https://drive.google.com/file/d/1Kt41KBox6SJB2774ykXkIj_cDoFTogWy/view?usp=sharing
+
+---
+
+## Descripción del programa
+
+El sistema permite:
+- Leer datos de países desde un archivo `datos_paises.csv`.
+- Buscar países por nombre (coincidencia parcial o exacta).
+- Filtrar países por continente, rango de población o superficie.
+- Ordenar países por nombre, población o superficie.
+- Calcular estadísticas generales (totales, promedios, máximos y mínimos).
+- Agregar, actualizar y eliminar registros de países.
+- Guardar los cambios realizados en el archivo CSV.
+
+El programa utiliza estructuras de datos como **listas y diccionarios**, junto con módulos estándar como `csv` y `os`.
+
+---
+
+## Instrucciones de uso
+
+### 1. Requisitos
+- Tener instalado **Python 3.10 o superior**.
+
+### 2. Estructura de archivos
+Asegúrate de tener en la misma carpeta:
+gestion_paises.py
+datos_paises.csv
+
+
+### 3. Ejecución
+Abrí una terminal en la carpeta del proyecto y ejecutá:
+```bash
+python gestion_paises.py
+```
+
+Aparecerá un menú principal con las siguientes opciones:
+```bash
+1) Buscar país por nombre
+2) Filtrar países
+3) Ordenar países
+4) Estadísticas de países
+5) Agregar nuevo país
+6) Actualizar datos de país
+7) Eliminar país
+8) Salir
+```
+
+Seguí las instrucciones en pantalla para interactuar con el programa.
+
+## Ejemplo del archivo CSV
+
+#### Formato inicial del archivo
+
+```csv
+nombre,continente,poblacion,superficie
+Argentina,America,45376763,2780400
+España,Europa,47450795,505990
+Japón,Asia,125800000,377975
+Brasil,America,213993437,8515767
+Australia,Oceania,25788217,7692024
+```
+
+## Ejemplos de uso
+
+### Ejemplo 1: Buscar país
+
+![Buscar un país](./capturas/buscar_pais.png)
+
+### Ejemplo 2: Filtrar países por continente
+
+![Filtrar países](./capturas/filtrar_pais.png)
+
+### Ejemplo 3: Estadísticas
+
+![Estadísticas países](./capturas/estadisticas.png)
+
+
+#### Materia: 
+Programación I - Universidad Tecnológica Nacional
+#### Integrantes:
+ - Facundo Zerbato
+ - Mauro Barua
